c1e527853bc48b580f2502e07c751cb4352ef894
OlehMalskyi <omalskij3@gmail.com>
2023-12-07 20:25:00 +0200

    lb10.3

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB10.3/LB10.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(Test1)
+		{
+			
+		}
+	};
+}
